cbda24033d5a03d14ae2b98d708f27090f3c8f4f
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-01-14 10:33:52 +0200

    zX

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const webpush = require("web-push");
+const bodyparser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "client")));
+
 const port = process.env.PORT || 5000;
+
+const pubKey =
+  "BDPknGIKMSNfFGKYU0UnoJJtgb33Zupuv2Lm-tb1n2ttd24mPMr7Ai_VUrSSbaBLbRTYcu2PTIhTj-Hu6qTMuzU";
+const prtKey = "Tphrzpar1nWjhY52s39ij0aRq_Jp1roGc-79s8wBx3A";
+
+webpush.setVapidDetails("mailto:pantsflow@gmail.com", pubKey, prtKey);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(bodyparser.json());
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server Running at localhost:${port}`);
 });
 app.use(
   cors({
@@ -20,6 +33,7 @@ app.use(
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
+//https://api.bscscan.com/api?module=account&action=tokentx&contractaddress=0x9505dbD77DaCD1F6C89F101b98522D4b871d88C5&address=0x1b6fD1C466d5b8380bF8eDc2b7C0a3fC1f892E56&page=1&offset=5&startblock=0&endblock=999999999&sort=asc&apikey=41ZFPTAU8UR5GAKUHQCEXG586N198FW2MH
 
 app.get("/thedata", async (req, res) => {
   try {
@@ -50,4 +64,14 @@ app.get("/thedata", async (req, res) => {
     console.error(err);
     res.status(500).send();
   }
+});
+
+app.post("/createnotf", (req, res) => {
+  const subs = req.body;
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({ title: "Push test" });
+
+  webpush.sendNotification(subs, payload).catch((err) => console.errror(er));
 });
